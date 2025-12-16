72316ceaf062cdf25a5b3713887f6e52b159370f
CallMeIlyas <gmuhammadilyasabdulghoni@gmail.com>
2025-12-16 11:17:30 +0700

    pesan bebas

--- vite.config.ts
+++ vite.config.ts
@@ -1,31 +1,32 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { resolve } from "path";
 import { fileURLToPath } from 'url';
-import { dirname, resolve } from 'path';
+import { dirname } from 'path';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: "/",
   server: {
     host: true,
   },
   assetsInclude: [
-    '**/*.png',
-    '**/*.jpg',
-    '**/*.jpeg',
-    '**/*.gif',
-    '**/*.mp4',
-    '**/*.svg',
-    '**/*.ttf',
-    '**/*.woff',
-    '**/*.woff2',
+    "**/*.png",
+    "**/*.jpg",
+    "**/*.jpeg", 
+    "**/*.gif",
+    "**/*.mp4",
+    "**/*.svg",
+    "**/*.ttf",
+    "**/*.woff",
+    "**/*.woff2",
   ],
   resolve: {
     alias: {
-      '/data/data/com.termux/files/home/folder/src': resolve(__dirname, 'src'),
+      "@": resolve(__dirname, "src"),
     },
   },
 });
